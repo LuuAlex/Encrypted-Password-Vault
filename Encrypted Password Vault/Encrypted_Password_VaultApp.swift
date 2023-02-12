a47a39225850d5a439f3877f9515d39e8e5272bb
Alex Luu <alexluu@berkeley.edu>
2023-02-12 11:04:17 -0800

    Initial Commit

---
+++ Encrypted Password Vault/Encrypted_Password_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Encrypted_Password_VaultApp.swift
+//  Encrypted Password Vault
+//
+//  Created by Alex Luu on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Encrypted_Password_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Encrypted Password VaultUITests/Encrypted_Password_VaultUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Encrypted_Password_VaultUITestsLaunchTests.swift
+//  Encrypted Password VaultUITests
+//
+//  Created by Alex Luu on 2/12/23.
+//
+
+import XCTest
+
+final class Encrypted_Password_VaultUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
