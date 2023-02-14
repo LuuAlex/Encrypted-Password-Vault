28b99582ead4980b3b1cb3c5c97ddca2cdf417d6
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2023-02-14 09:18:33 -0800

    small changes

--- Encrypted Password Vault/Encrypted_Password_VaultApp.swift
+++ Encrypted Password Vault/Encrypted_Password_VaultApp.swift
@@ -19,10 +19,11 @@ struct Encrypted_Password_VaultApp: App {
 
 
 func runCreateCSV(path: String, password: String) {
-    PythonLibrary.useLibrary(at: "/Users/alexluu/Documents/GitHub/Encrypted Password Vault/Encrypted Password Vault/.venv/lib/python3.10")
+    PythonLibrary.useLibrary(at: "/Users/alexluu/Documents/GitHub/Encrypted Password Vault/Encrypted Password Vault/.venv/bin/python3")
     let os = Python.import("os")
     let base64 = Python.import("base64")
     let csv = Python.import("csv")
+    let cryptography = Python.import("cryptography")
     let Fernet = Python.import("cryptography.fernet")
     let hashes = Python.import("cryptography.hazmat.primitives")
     let PBKDF2HMAC = Python.import("ryptography.hazmat.primitives.kdf.pbkdf2")
@@ -35,10 +36,11 @@ func runCreateCSV(path: String, password: String) {
 }
 
 func runRead(path: String, password: String) -> [DataObject] {
-    PythonLibrary.useLibrary(at: "/Users/alexluu/Documents/GitHub/Encrypted Password Vault/Encrypted Password Vault/.venv/lib/python3.10")
+    PythonLibrary.useLibrary(at: "/Users/alexluu/Documents/GitHub/Encrypted Password Vault/Encrypted Password Vault/.venv/bin/python3")
     let os = Python.import("os")
     let base64 = Python.import("base64")
     let csv = Python.import("csv")
+    let cryptography = Python.import("cryptography")
     let Fernet = Python.import("cryptography.fernet")
     let hashes = Python.import("cryptography.hazmat.primitives")
     let PBKDF2HMAC = Python.import("ryptography.hazmat.primitives.kdf.pbkdf2")
